37b19b5774698e677e6985e4b3d663bcb2ddc91e
Fabrizzio <fbrzzochoa@gmail.com>
2025-10-18 15:42:36 -0500

    Add files via upload

--- LP3_LAB_7/src/Actividad5/AppContactos.java
+++ LP3_LAB_7/src/Actividad5/AppContactos.java
@@ -1,5 +1,12 @@
+/*************************************************************************************************
+ARCHIVO		: AppContactos.java
+FECHA		: 15/10/2025
+*************************************************************************************************/
 package Actividad5;
 
 public class AppContactos {
-
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        agenda.bucle();
+    }
 }
--- LP3_LAB_7/src/Actividad5/Persona.java
+++ LP3_LAB_7/src/Actividad5/Persona.java
@@ -1,5 +1,46 @@
+/*************************************************************************************************
+ARCHIVO		: Persona.java
+FECHA		: 15/10/2025
+*************************************************************************************************/
 package Actividad5;
 
 public class Persona {
-
+	protected String nombre;
+	protected String telefono;
+	protected String direccion;
+	
+	public Persona(String nombre, String telefono, String direccion) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String toString() {
+		return this.nombre + "\t" + this.telefono + "\t" + this.direccion + "\n";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
